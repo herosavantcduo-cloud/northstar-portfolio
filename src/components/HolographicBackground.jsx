8c4b1346172f4a8dcce8bef0dcfd00f87f957bcb
william g cox <herosavantcduo@gmail.com>
2026-03-10 00:32:48 +0000

    File changes

--- src/components/HolographicBackground.jsx
+++ src/components/HolographicBackground.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
 
-export default function HolographicBackground({ videoId = "jfKfPfyJRdk" }) {
+export default function HolographicBackground({ videoId = "HGgQpXDIjsw" }) {
   const mountRef = useRef(null);
 
   useEffect(() => {
